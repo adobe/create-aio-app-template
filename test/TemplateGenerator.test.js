7553bd29af40dd801f22717626869fab11f8293d
Shazron Abdullah <shazron@gmail.com>
2022-02-09 11:47:32 +0800

    Revert "not sure what is going on just partitioning the problem :^O"
    This reverts commit b39d67520413faa964f5272a1f67cbd88818d3f3.

--- test/TemplateGenerator.test.js
+++ test/TemplateGenerator.test.js
@@ -23,10 +23,8 @@ describe('prototype', () => {
 describe('run', () => {
   test('test generator', async () => {
     const options = { 'template-name': 'my-template' }
-    console.log('theGeneratorPath', theGeneratorPath)
-    expect(1).toEqual(1)
 
-    // const ret = await helpers.run(theGeneratorPath)
-    // expect(ret).toBeDefined()
+    const ret = await helpers.run(theGeneratorPath)
+    expect(ret).toBeDefined()
   })
 })
