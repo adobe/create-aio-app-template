288f4a3a0fc3501849f06525e01e09495a10ae6f
Shazron Abdullah <shazron@gmail.com>
2022-08-19 16:55:28 +0800

    experiment 2: remove capture of stdout/stderr

--- test/jest.setup.js
+++ test/jest.setup.js
@@ -1,17 +1,17 @@
-const { stdout, stderr } = require('stdout-stderr')
+// const { stdout, stderr } = require('stdout-stderr')
 
 process.on('unhandledRejection', error => {
   throw error
 })
 
 // trap console log
-beforeEach(() => {
-  stdout.start()
-  stderr.start()
-  stdout.print = false // set to true to see output
-})
+// beforeEach(() => {
+//   stdout.start()
+//   stderr.start()
+//   stdout.print = false // set to true to see output
+// })
 
-afterEach(() => {
-  stdout.stop()
-  stderr.stop()
-})
+// afterEach(() => {
+//   stdout.stop()
+//   stderr.stop()
+// })
